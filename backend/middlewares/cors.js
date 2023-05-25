38029145849c0b3528a9d3771563e4abaf7ac18e
Rustam Malikov <mrj@lowcost-mebel.ru>
2023-05-26 00:02:03 +0300

    ref: изменить cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -14,7 +14,7 @@ const cors = (req, res, next) => {
   }
   res.header('Access-Control-Allow-Origin', origin);*/
 
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://nishima42.students.nomoredomains.monster');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.header('Access-Control-Allow-Credentials', 'true');
