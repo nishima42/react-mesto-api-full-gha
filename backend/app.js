d29b05eb5b9bcabeaca0d90ce882c49c5731638f
Rustam Malikov <mrj@lowcost-mebel.ru>
2023-05-26 15:57:01 +0300

    вынести dbURL в окружение

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
+const helmet = require('helmet');
 const { cors } = require('./middlewares/cors');
 
 const router = require('./routes/index');
@@ -20,12 +22,13 @@ const {
   errorLogger,
 } = require('./middlewares/logger');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const app = express();
 
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+mongoose.connect(DB_URL);
 
+app.use(helmet());
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(requestLogger);
